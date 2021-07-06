dbd76cb0da838efd37bceba60aaf4c64a669fa77
nasywa554 <nasywanurnafilah554@gmail.com>
2021-07-06 10:02:35 +0700

    create view halaman baru

---
+++ resources/views/halaman_baru.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">halaman baru</div>
+
+                <div class="card-body">
+                   
+                       
+
+                    
+
+                    <table border="1">
+                        <tr>
+                            <td>id</td>
+                            <td>Username</td>
+                            <td>Gmail</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>nama</td>
+                            <td>nama@gmail.com</td>
+                        </tr>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
